99e4b1d1e207320682649e503d0aee4ba6b20c8d
Overone1957 <eowgao1155@gmail.com>
2022-11-08 22:46:55 -0500

    Update Lab9Challenge2.js

--- Lab9/Lab9Challenge2.js
+++ Lab9/Lab9Challenge2.js
@@ -1,7 +1,7 @@
 var input=prompt("What is your age?");
 console.log(input);
 var today = new Date();
-var year = getFullYear();
+var year = today.getFullYear();
 console.log(year);
 var birth = year.getFullYear() - Number(input);
 document.getElementById('birthyear').innerHTML = birth;
