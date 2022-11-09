60cd7d90e192e38c20cfee8aa0338039c2d8cc1e
Overone1957 <eowgao1155@gmail.com>
2022-11-08 20:07:55 -0500

    Update Lab9Challenge1.js

--- Lab9/Lab9Challenge1.js
+++ Lab9/Lab9Challenge1.js
@@ -1,7 +1,6 @@
 function stopTime(){
   var start = new Date();
   var stop = new Date();
-  var output = stop.getTime() - start.getTime();
-  var outputsecond = output / 600;
-  alert("You have been on the page for: "+outputsecond);
+  var output = (stop.getTime() - start.getTime())/600;
+  alert("You have been on the page for: "+output);
 }
