1db6e85544ed12ec60a204bd483f14051041c62f
Overone1957 <eowgao1155@gmail.com>
2022-11-15 18:10:37 -0500

    Update Lab10Challenge.js

--- Lab10/Lab10Challenge.js
+++ Lab10/Lab10Challenge.js
@@ -1,6 +1,25 @@
-function add(){
+function math(operator){
   var number1 = Number(document.getElementById('num1').value);
   var number2 = Number(document.getElementById('num2').value);
-  var output = number1 + number2;
-  document.getElementById('output').innerHTML = output;
+
+  if (operator=="+"){
+    console.log("Add");
+    output = number1 + number2;
+    document.getElementById("output").value = output;
+  }
+  if (operator=="-"){
+    console.log("Subtract");
+    output = number1 - number2;
+    document.getElementById("output").value = output;
+  }
+  if (operator=="*"){
+    console.log("Multiply");
+    output = number1 * number2;
+    document.getElementById("output").value = output;
+  }
+  if (operator=="/"){
+    console.log("Divide");
+    output = number1 / number2;
+    document.getElementById("output").value = output;
+  }
 }
