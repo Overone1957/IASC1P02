d6bed6662905dc99ef2065494fcb45b8cf3b3293
Overone1957 <eowgao1155@gmail.com>
2022-12-02 18:29:33 -0500

    1

--- Assignment4/Assignment4Page1.js
+++ Assignment4/Assignment4Page1.js
@@ -1,3 +1,22 @@
 function Welcome(){
   alert ("Welcome to the world of Overwatch!")
 }
+
+var second = 0;
+var minute = 0;
+var hour = 0;
+window.setTimeout("interval();", 1000);
+function interval() {
+  second++;
+  if (second == 60) {
+    second = 0;
+    minute += 1;
+  }
+  if (minute == 60) {
+    minute = 0;
+    hour += 1;
+  }
+  window.setTimeout("interval();", 1000);
+  var time = hour + " hour " + minute + " minute " + second + " second"
+  document.getElementById('time').innerHTML = time;
+}
